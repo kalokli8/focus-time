89aa58ba7b38e2b9249c5412260ec9a81399d6cb
kalokli8 <gelu13579@hotmail.com.hk>
2021-03-27 12:05:07 +0800

    justifyContentCenter

--- src/features/timer/Timing.js
+++ src/features/timer/Timing.js
@@ -1,33 +1,45 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
-import {RoundedButton} from '../../components/RoundedButton';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { RoundedButton } from "../../components/RoundedButton";
 
-export const Timing = ({onChangeTime}) => {
+export const Timing = ({ onChangeTime }) => {
   return (
     <>
-    <View style={styles.timingButton}>
-    <View style={styles.buttonContainer}>
-    <RoundedButton size={75} title="10" onPress={() => onChangeTime(10)} />
-    </View>
-    <View style={styles.buttonContainer}>
-    <RoundedButton size={75} title="15" onPress={() => onChangeTime(15)} />
-    </View>
-    <View style={styles.buttonContainer}>
-    <RoundedButton size={75} title="20" onPress={() =>onChangeTime(20)} />
-    </View>
-    </View>
+      <View style={styles.timingButton}>
+        <View style={styles.buttonContainer}>
+          <RoundedButton
+            size={75}
+            title="10"
+            onPress={() => onChangeTime(10)}
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <RoundedButton
+            size={75}
+            title="15"
+            onPress={() => onChangeTime(15)}
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <RoundedButton
+            size={75}
+            title="20"
+            onPress={() => onChangeTime(20)}
+          />
+        </View>
+      </View>
     </>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   timingButton: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    
-  
-  }, buttonContainer: {
-    marginRight: 10
-  }
-})
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    marginRight: 10,
+  },
+});
